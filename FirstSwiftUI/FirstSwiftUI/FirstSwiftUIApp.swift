a935955dc8d34c829d83434c5642ee088128678a
ashdaviss <cuu19@txstate.edu>
2026-03-25 12:55:23 -0500

    Add files via upload

---
+++ FirstSwiftUI/FirstSwiftUI/FirstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIApp.swift
+//  FirstSwiftUI
+//
+//  Created by Davis, Ashlyn on 3/25/26.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
